cfc4881dc65197b86b3c617168139e8c9ef4cb9d
Xinxin Xue <xxue@zoology.ubc.ca>
2017-03-15 20:01:49 -0700

    Fix for port and procfile

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(function(err, req, res, next) {
   res.render("error")
 })
 
-const PORT = 3500
+const PORT = process.env.PORT || 3500
 app.listen(PORT, function() { console.log("Listening on port: " + PORT)})
 module.exports = app
 
